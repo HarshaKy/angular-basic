3526ab8e7f63cfb9cf4505e1655adf9512d090c8
HarshaKy <harshaky6573@gmail.com>
2020-08-24 11:07:24 +0530

    get users and display on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	users
 
-  ngOnInit(): void {
-  }
+	constructor(private data: DataService) { }
+
+	ngOnInit(): void {
+		this.data.getUsers().subscribe(data => {
+			this.users = data
+			console.log(this.users)
+		})
+	}
 
 }
